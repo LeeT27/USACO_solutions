b6dc18c1e32f471bca89bb4a01928d27e90ffec8
Juno <138246438+LeeT27@users.noreply.github.com>
2025-02-28 11:09:29 -0600

    Update teamTicTacToe.cpp

--- teamTicTacToe.cpp
+++ teamTicTacToe.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
 #include <vector>
-int single(){
-    return 2;
-}
-int team(){
-    return 3;
-}
+
 int main(){
     freopen("tttt.in", "r", stdin);
 	freopen("tttt.out", "w", stdout);
+
     std::vector<std::vector<char>> board(3, std::vector<char>(3, 0));
     for (int i=0;i!=3;i++){
         std::cin>>board[i][0]>>board[i][1]>>board[i][2];
     }
-    std::cout<<single()<<"\n";
-    std::cout<<team()<<"\n";
+
+    int single{0};
+    int team{0};
+    //checks for row win
+    for (int i=0;i!=3;i++){
+        
+        //checks for row win
+        if (board[i][0]==board[i][1]&&board[i][2]==board[i][0]){
+            single++;
+        }
+        else if (board[i][0]==board[i][1]||board[i][1]==board[i][2]||board[i][0]==board[i][2]){
+            team++;
+        }
+        //checks for column win
+        if (board[0][i]==board[1][i]&&board[2][i]==board[0][i]){
+            single++;
+        }
+        else if (board[0][i]==board[1][i]||board[1][i]==board[2][i]||board[0][i]==board[2][i]){
+            team++;
+        }
+    }
+    //checks for diagonal wins
+    if (board[0][0]==board[1][1]&&board[0][0]==board[2][2]){
+        single++;
+    }
+    else if (board[0][0]==board[1][1]||board[1][1]==board[2][2]||board[0][0]==board[2][2]){
+        team++;
+    }
+    if (board[0][2]==board[1][1]&&board[2][0]==board[1][1]){
+        single++;
+    }
+    else if (board[0][2]==board[1][1]||board[1][1]==board[2][0]||board[0][2]==board[2][0]){
+        team++;
+    }
+
+    std::cout<<single<<"\n";
+    std::cout<<team<<"\n";
     return 0;
 }
