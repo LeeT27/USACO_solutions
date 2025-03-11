a029f96452ed98af6e78d1f0b880e7bf455c43c7
Juno <138246438+LeeT27@users.noreply.github.com>
2025-03-11 11:12:21 -0500

    Update #8_triangles.cpp

--- #8_triangles.cpp
+++ #8_triangles.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <vector>
 
+int crossProduct(int x1, int y1, int x2, int y2, int x3, int y3){
+    int vx1 = x2 - x1;
+    int vy1 = y2 - y1;
+    int vx2 = x3 - x1;
+    int vy2 = y3 - y1;
+    return (vx1 * vy2) - (vy1 * vx2);
+}
+
 int main(){
     freopen("triangles.in", "r", stdin);
 	freopen("triangles.out", "w", stdout);
 
-    int crossProduct(int x1, int y1, int x2, int y2, int x3, int y3) {
-        // Form two vectors: A->B and A->C
-        int vx1 = x2 - x1;
-        int vy1 = y2 - y1;
-        int vx2 = x3 - x1;
-        int vy2 = y3 - y1;
-        
-        // Compute the cross product
-        return (vx1 * vy2) - (vy1 * vx2);
-    }
-
     int N{};
+    int max{0};
+    int temp{};
+
     std::cin>>N;
     std::vector<int> xCoor(N, 0);
     std::vector<int> yCoor(N, 0);
@@ -24,6 +24,16 @@ int main(){
     for (int i=0;i!=N;i++){
         std::cin>>xCoor[i]>>yCoor[i];
     }
-    
+    for (int i=0;i!=N;i++){
+        for (int j=0;j!=N;j++){
+            for (int k=0;k!=N;k++){
+                temp=crossProduct(xCoor[i], yCoor[i], xCoor[j], yCoor[j], xCoor[k], yCoor[k]);
+                if (temp>max){
+                    max=temp;
+                }
+            }
+        }
+    }
+    std::cout<<max;
     return 0;
 }
