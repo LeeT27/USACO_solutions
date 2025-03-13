c5725cd077c86353ad94c8e874a0d8bed01b27d8
Juno <138246438+LeeT27@users.noreply.github.com>
2025-03-13 11:03:06 -0500

    Update #8_triangles.cpp

--- #8_triangles.cpp
+++ #8_triangles.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
 #include <vector>
 
-int crossProduct(int x1, int y1, int x2, int y2, int x3, int y3){
-    int vx1 = x2 - x1;
-    int vy1 = y2 - y1;
-    int vx2 = x3 - x1;
-    int vy2 = y3 - y1;
-    return (vx1 * vy2) - (vy1 * vx2);
-}
-
 int main(){
     freopen("triangles.in", "r", stdin);
 	freopen("triangles.out", "w", stdout);
 
     int N{};
     int max{0};
-    int temp{};
+    int area{};
 
     std::cin>>N;
     std::vector<int> xCoor(N, 0);
@@ -27,9 +19,11 @@ int main(){
     for (int i=0;i!=N;i++){
         for (int j=0;j!=N;j++){
             for (int k=0;k!=N;k++){
-                temp=crossProduct(xCoor[i], yCoor[i], xCoor[j], yCoor[j], xCoor[k], yCoor[k]);
-                if (temp>max){
-                    max=temp;
+                if (xCoor[i] == xCoor[j] && yCoor[i] == yCoor[k]) { 
+                    area=std::abs(xCoor[k] - xCoor[i]) * std::abs(yCoor[j] - yCoor[i]);;
+                    if (area>max){
+                        max=area;
+                    }
                 }
             }
         }
